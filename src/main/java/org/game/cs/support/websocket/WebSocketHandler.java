f6cda489c602eb6c39282c6c3788c68bf94f6189
Krisztian Horvath <keyki.kk@gmail.com>
2013-01-18 17:34:58 +0100

    test

--- src/main/java/org/game/cs/support/websocket/WebSocketHandler.java
+++ src/main/java/org/game/cs/support/websocket/WebSocketHandler.java
@@ -65,6 +65,7 @@ public class WebSocketHandler implements Handler<ServerWebSocket>, Observer {
 
     @Override
     public void update(LogEvent event) {
+        LOGGER.debug("PINA:"+event.getSender());
         LOGGER.info("event received: " + event.getMessage() + " from: " + event.getSender());
         List<ServerWebSocket> webSocketList = getWebSocketList(event.getSender());
         for (ServerWebSocket webSocket : webSocketList) {
